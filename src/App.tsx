d04b05015196aa0b4ae2799fea7d9aec656f095a
AlexanderMokeichuk <alexander.q.qa.a@mail.ru>
2024-01-09 16:37:23 +0600

    Added function opening cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
+import CreateItems from "./components/createItems/createItems.ts";
+import {Cube} from "./types";
+import DrawBoard from "./components/DrawBoard/DrawBoard.tsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cube, setCube] = useState<Cube[]>(CreateItems());
+
+  const openCube = (id:number) => {
+    const cubesCopy = [...cube];
+    const cubeCopy = {...cubesCopy[id]};
+    cubeCopy.clicked = true;
+    cubesCopy[id] = cubeCopy;
+    setCube(cubesCopy);
+  }
+
+  const resetGame = () => {
+    setCube(CreateItems());
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={"container"}>
+          {cube.map((cube) => {
+            return (<DrawBoard
+              key={cube.id}
+              id={cube.id}
+              hasItem={cube.hasItem}
+              clicked={cube.clicked}
+              openCube={() => openCube(cube.id)}
+            >
+            </DrawBoard>);
+          })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={resetGame}>Reset</button>
     </>
   )
 }
 
-export default App
+export default App;
